d7cad579a9ea730a303463c8140f2b79601d6409
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-01-13 11:08:19 +0000

    Add versioneer and black

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=requirements,
 )

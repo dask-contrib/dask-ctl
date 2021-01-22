dde5c21f2be92a7d8860ef5e038fb436e0e3c24a
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-01-22 10:56:03 +0000

    Expose things at top level

--- dask_ctl/__init__.py
+++ dask_ctl/__init__.py
@@ -2,3 +2,13 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from .discovery import (
+    discover_cluster_names,
+    discover_clusters,
+    list_clusters,
+    get_cluster,
+    scale_cluster,
+    delete_cluster,
+)
+from .proxy import ProxyCluster

3e1e41d9113650c7647090dfa5620a53924aab2e
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-03-11 11:13:10 +0000

    [CI DEBUG] Bump timeout to 10 seconds

--- dask_ctl/tests/test_discovery.py
+++ dask_ctl/tests/test_discovery.py
@@ -33,7 +33,7 @@ async def test_cluster_client():
     port = 8786
     async with LocalCluster(scheduler_port=port, asynchronous=True) as _:
         async with Client(
-            f"tcp://localhost:{port}", asynchronous=True, timeout=0.5
+            f"tcp://localhost:{port}", asynchronous=True, timeout=10
         ) as client:
             assert int(client.scheduler.address.split(":")[-1]) == port
 

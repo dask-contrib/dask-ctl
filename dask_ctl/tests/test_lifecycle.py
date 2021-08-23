37189f8b2d71143e8188e9321925c55301d0e9be
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-08-23 16:27:10 +0100

    Split out tests for async isolation

--- dask_ctl/tests/test_lifecycle.py
+++ dask_ctl/tests/test_lifecycle.py
@@ -1,10 +1,8 @@
 import pytest
 
-import ast
-
 from dask.distributed import LocalCluster
 
-from dask_ctl.lifecycle import create_cluster, get_snippet
+from dask_ctl.lifecycle import create_cluster
 
 
 @pytest.mark.asyncio
@@ -12,14 +10,3 @@ async def test_create_cluster(simple_spec_path):
     cluster = await create_cluster(simple_spec_path)
 
     assert isinstance(cluster, LocalCluster)
-
-
-def test_snippet():
-    with LocalCluster(scheduler_port=8786) as _:
-        snippet = get_snippet("proxycluster-8786")
-
-        # Check is valid Python
-        ast.parse(snippet)
-
-        assert "ProxyCluster" in snippet
-        assert "proxycluster-8786" in snippet

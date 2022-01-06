4feb1776a5f307fd704706e6e6f54aa0adbed6f3
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-01-06 13:36:48 +0000

    Update to use refactored RPC method and some refactoring

--- dask_ctl/tui/widgets/progress.py
+++ dask_ctl/tui/widgets/progress.py
@@ -1,5 +1,3 @@
-import random
-
 from rich.table import Table
 from rich.progress import Progress
 
@@ -8,9 +6,8 @@ from textual.app import App
 from textual.reactive import Reactive
 from textual.widget import Widget
 from rich.panel import Panel
-from dask.distributed import client
 from distributed.core import rpc
-import asyncio
+
 
 class Progress(Widget):
     progress = Reactive(Progress(auto_refresh=False, expand=True))
@@ -20,19 +17,12 @@ class Progress(Widget):
         # won't start correctly without a single task?
         self.task1 = self.progress.add_task("Task...", total=100, visible=False)
         self.tasks = {}
-        self.set_interval(0.5, self.generate_data)
         self.set_interval(0.3, self.get_progress)
         self.r = None
 
-    async def generate_data(self):
-        self.progress.update(self.task1, completed=100)
-        self._tick = not self._tick
-
-
     async def get_progress(self):
-        state, totals = await self.get_task_groups()
-        # for r, m, e, p, a, l in zip(
-        names = sorted(state["all"], key=state["all"].get, reverse=True)
+        state = await self.get_task_groups()
+        names = list(state.keys())
 
         # remove computed/deleted tasks
         keys = list(self.tasks.keys())
@@ -41,52 +31,32 @@ class Progress(Widget):
                 t = self.tasks.pop(name)
                 self.progress.remove_task(t)
 
-
         # add progress bars for new tasks
-        for name in names:
+        for name in reversed(names):
             if name not in self.tasks.keys():
-                a = state['all'][name]
+                a = state[name]["all"]
                 self.log(a, name)
                 self.tasks[name] = self.progress.add_task(name, total=a)
             t = self.tasks[name]
-            totals = state['released'][name] + state['memory'][name]
+            totals = state[name]["released"] + state[name]["memory"]
             self.log(totals, name)
             self.progress.update(t, completed=totals)
 
-        task_names = [t.description for t in self.progress.tasks]
         self._tick = not self._tick
 
     async def get_task_groups(self):
-        if hasattr(self.app, 'cluster'):
-            self.r = self.app.cluster.scheduler_comm if self.r is None else self.r
-            foo = await self.r.task_prefix()
-        # probably in testing mode with a localcluster on 8786
-        else:
-            self.r = rpc("localhost:8786") if self.r is None else self.r
-        state = await self.r.task_prefix()
+        if self.r is None:
+            if hasattr(self.app, "cluster"):
+                self.r = self.app.cluster.scheduler_comm
+            # probably in testing mode with a localcluster on 8786
+            else:
+                self.r = rpc("localhost:8786")
 
-        state["all"] = {
-                k: sum(v[k] for v in state.values()) for k in state["memory"]
-            }
+        state = await self.r.get_task_prefix_states()
 
-        totals = {
-            k: sum(state[k].values())
-            for k in ["all", "memory", "erred", "released", "waiting"]
-        }
-
-        totals["processing"] = totals["all"] - sum(
-            v for k, v in totals.items() if k != "all"
-        )
-
-        #     self.root.title.text = (
-        #         "Progress -- total: %(all)s, "
-        #         "in-memory: %(memory)s, processing: %(processing)s, "
-        #         "waiting: %(waiting)s, "
-        #         "erred: %(erred)s" % totals
-        #     )
-        # task_groups
-        return (state, totals)
-
+        for group in state:
+            state[group]["all"] = sum(state[group].values())
+        return state
 
     def render(self) -> Table:
         self.progress.refresh()

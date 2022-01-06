f8d33f933242cb71f5c3c24d9619a6bb9bc998e6
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-01-06 14:48:35 +0000

    Tweak progress styling

--- dask_ctl/tui/widgets/progress.py
+++ dask_ctl/tui/widgets/progress.py
@@ -1,28 +1,45 @@
+import rich.box
 from rich.table import Table
-from rich.progress import Progress
+from rich.progress import (
+    Progress as RichProgress,
+    BarColumn,
+    TimeRemainingColumn,
+)
 
 from textual import events
 from textual.app import App
 from textual.reactive import Reactive
 from textual.widget import Widget
 from rich.panel import Panel
-from distributed.core import rpc
+
+from ... import get_cluster
 
 
 class Progress(Widget):
-    progress = Reactive(Progress(auto_refresh=False, expand=True))
-    _tick = Reactive(False)
+    state = Reactive({})
+    title = "Progress"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        bar_column = BarColumn(bar_width=None)
+        self.progress = RichProgress(
+            "[white]{task.description}[/white]",
+            bar_column,
+            "[white]{task.completed}/{task.total}[/white]",
+            TimeRemainingColumn(),
+            auto_refresh=False,
+            expand=True,
+        )
 
     async def on_mount(self, event):
         # won't start correctly without a single task?
         self.task1 = self.progress.add_task("Task...", total=100, visible=False)
         self.tasks = {}
         self.set_interval(0.3, self.get_progress)
-        self.r = None
 
     async def get_progress(self):
-        state = await self.get_task_groups()
-        names = list(state.keys())
+        self.state = await self.get_task_groups()
+        names = list(self.state.keys())
 
         # remove computed/deleted tasks
         keys = list(self.tasks.keys())
@@ -34,36 +51,50 @@ class Progress(Widget):
         # add progress bars for new tasks
         for name in reversed(names):
             if name not in self.tasks.keys():
-                a = state[name]["all"]
-                self.log(a, name)
+                a = self.state[name]["total"]
                 self.tasks[name] = self.progress.add_task(name, total=a)
             t = self.tasks[name]
-            totals = state[name]["released"] + state[name]["memory"]
-            self.log(totals, name)
-            self.progress.update(t, completed=totals)
+            total = self.state[name]["released"] + self.state[name]["memory"]
+            self.progress.update(t, completed=total)
 
-        self._tick = not self._tick
+        totals = {}
+        for task in self.state:
+            for state in self.state[task]:
+                if state in totals:
+                    totals[state] += self.state[task][state]
+                else:
+                    totals[state] = self.state[task][state]
+
+        # Update title
+        if self.tasks:
+            self.title = "Progress \u2500\u2500 " + ", ".join(
+                [f"{key}: {value}" for key, value in totals.items()]
+            )
+        else:
+            self.title = "Progress"
 
     async def get_task_groups(self):
-        if self.r is None:
-            if hasattr(self.app, "cluster"):
-                self.r = self.app.cluster.scheduler_comm
-            # probably in testing mode with a localcluster on 8786
-            else:
-                self.r = rpc("localhost:8786")
-
-        state = await self.r.get_task_prefix_states()
+        state = await self.app.cluster.scheduler_comm.get_task_prefix_states()
 
         for group in state:
-            state[group]["all"] = sum(state[group].values())
+            state[group] = {"total": sum(state[group].values()), **state[group]}
         return state
 
     def render(self) -> Table:
         self.progress.refresh()
-        return Panel(self.progress)
+        return Panel(
+            self.progress,
+            title=self.title,
+            title_align="left",
+            style="blue",
+            box=rich.box.SQUARE,
+        )
 
 
 class Demo(App):
+
+    cluster = get_cluster("proxycluster-8786")
+
     async def on_load(self, event: events.Load) -> None:
         await self.bind("q", "quit", "Quit")
 

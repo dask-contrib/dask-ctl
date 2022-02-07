967a1e61c890232194b1b5ec0b6d0bd35a3e2fce
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-02-07 10:38:19 +0000

    Give cluster space to breathe in case this test has a race condition

--- dask_ctl/tests/test_lifecycle.py
+++ dask_ctl/tests/test_lifecycle.py
@@ -1,6 +1,6 @@
 import ast
 
-from dask.distributed import LocalCluster
+from dask.distributed import LocalCluster, Client
 
 from dask_ctl.lifecycle import create_cluster, get_snippet
 
@@ -12,7 +12,10 @@ async def test_create_cluster(simple_spec_path):
 
 
 def test_snippet():
-    with LocalCluster(scheduler_port=8786) as _:
+    with LocalCluster(scheduler_port=8786) as cluster:
+        client = Client(cluster)
+        client.wait_for_workers(1)
+
         snippet = get_snippet("proxycluster-8786")
 
         # Check is valid Python

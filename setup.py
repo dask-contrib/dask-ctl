3d12a3fba5e0e1322936276c7b2955384614cc2f
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2022-03-07 13:10:05 +0000

    Drop Python 3.7 (#35)
    * Drop Python 3.7
    
    * Remove stray character

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=requirements,
     entry_points="""
         [console_scripts]

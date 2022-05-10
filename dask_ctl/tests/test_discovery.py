5bc94c04d61491ee3f9f08aa855a455df576cfdf
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-05-10 15:04:28 +0100

    Pin port for macos

--- dask_ctl/tests/test_discovery.py
+++ dask_ctl/tests/test_discovery.py
@@ -9,6 +9,8 @@ from dask_ctl.discovery import (
     list_discovery_methods,
 )
 
+SCHEDULER_PORT = 8786
+
 
 def test_discovery_methods():
     assert "proxycluster" in list_discovery_methods()
@@ -17,7 +19,7 @@ def test_discovery_methods():
 @pytest.mark.asyncio
 async def test_discover_cluster_names():
     assert isinstance(discover_cluster_names(), AsyncIterator)
-    async with LocalCluster(scheduler_port=8786, asynchronous=True) as _:
+    async with LocalCluster(scheduler_port=SCHEDULER_PORT, asynchronous=True) as _:
         count = 0
         async for _ in discover_cluster_names():
             count += 1
@@ -28,26 +30,24 @@ async def test_discover_cluster_names():
 async def test_cluster_client():
     from dask.distributed import Client
 
-    port = 8786
-    async with LocalCluster(scheduler_port=port, asynchronous=True) as _:
+    async with LocalCluster(scheduler_port=SCHEDULER_PORT, asynchronous=True) as _:
         async with Client(
-            f"tcp://localhost:{port}", asynchronous=True, timeout=1
+            f"tcp://localhost:{SCHEDULER_PORT}", asynchronous=True, timeout=1
         ) as client:
-            assert int(client.scheduler.address.split(":")[-1]) == port
+            assert int(client.scheduler.address.split(":")[-1]) == SCHEDULER_PORT
 
 
 @pytest.mark.asyncio
 async def test_discovery_list():
     from dask_ctl.proxy import discover
 
-    port = 8786
-    async with LocalCluster(scheduler_port=port, asynchronous=True) as _:
+    async with LocalCluster(scheduler_port=SCHEDULER_PORT, asynchronous=True) as _:
         async for name, _ in discover():
-            assert str(port) in name
+            assert str(SCHEDULER_PORT) in name
 
 
 @pytest.mark.asyncio
 async def test_discover_clusters():
-    with LocalCluster() as cluster:
+    with LocalCluster(scheduler_port=SCHEDULER_PORT) as cluster:
         discovered_names = [c.name async for c in discover_clusters()]
         assert cluster.name in discovered_names

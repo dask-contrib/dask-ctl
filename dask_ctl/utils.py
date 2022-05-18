aad6cf11cd6dad64edf0c558f0c1876e6fcaa84a
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-05-18 14:12:38 +0100

    Remove signal handlers

--- dask_ctl/utils.py
+++ dask_ctl/utils.py
@@ -1,11 +1,9 @@
 import asyncio
 
 from tornado.ioloop import IOLoop
-from distributed.cli.utils import install_signal_handlers
 
 
 loop = IOLoop.current()
-install_signal_handlers(loop)
 
 
 class _AsyncTimedIterator:

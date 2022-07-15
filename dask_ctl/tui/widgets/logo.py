fdd516aee39a1dfa692a15651f02290b8de37163
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-07-15 17:35:42 -0500

    Add demo app to logo file

--- dask_ctl/tui/widgets/logo.py
+++ dask_ctl/tui/widgets/logo.py
@@ -1,4 +1,7 @@
 from rich.text import Text
+
+from textual import events
+from textual.app import App
 from textual.widget import Widget
 
 
@@ -16,3 +19,18 @@ class Logo(Widget):
         A╨\"""",
             style="orange3",
         )
+
+
+class Demo(App):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_load(self, event: events.Load) -> None:
+        await self.bind("q", "quit", "Quit")
+
+    async def on_mount(self, event: events.Mount) -> None:
+        await self.view.dock(Logo(), edge="top")
+
+
+if __name__ == "__main__":
+    Demo.run(title="Logo", log="textual.log")
